2cb710780db8a4144189d93daabcce277d34ce07
Adam_VA <adan_gonzalez@vibeadventures.com>
2025-05-14 11:21:38 -0700

    primer commit

---
+++ app/Http/Controllers/PSController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PSController extends Controller
+{
+    public function index(){
+        return 'entro';
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\PSController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/listPrices',[PSController::class,'index']);
